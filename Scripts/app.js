016316a7b6a47b1b28c8412aacccbd7a464b0fde
ifotn <rich@infrontofthenet.com>
2016-02-18 11:55:50 -0500

    End of class commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,72 +2,35 @@
 
 // setup your IIFE (Immediately Invoked Function Expression)
 (function () {
-
-
-    var paragraphData = [{"paragraph":"First Paragraph Stuff"}, 
-                        {"paragraph":"Second Paragraph Stuff"},
-                        {"paragraph":"Third Paragraph Stuff"},
-                        {"paragraph":"Fourth Paragraph Stuff"}];
-
-    console.log("App Started..."); 
-  
-  /* the ugly way
-    console.log(paragraphData[1]);
-    console.log(paragraphData[1]);
-    console.log(paragraphData[2]);
-    console.log(paragraphData[0]);
-    console.log(paragraphData[5]);
-    console.log(paragraphData[6]);
-  */
-  
-    // a little nicer but counting up
     
-    var paragraphDataLength = paragraphData.length;
-    for(var index= 0; index < paragraphDataLength; index++) {
-        console.log(paragraphData[index].paragraph);
+    if (document.getElementById("about") != null) {
+        var aboutParagraph = document.getElementById("aboutParagraph");
+        aboutParagraph.innerHTML = "This is all about me";
     }
-    
-    var name = document.getElementById("name");
-    
-    var button = document.getElementById("Button1");
-    
-    button.addEventListener("click", function(){
-        // execute stuff here after button click
-        console.log("Clicked!!");
-        console.log(name.value);
-    });
-    
-   
-     // a little nicer but counting down
-    /*
-    for(var index= paragraphData.length-1; index > -1; index--) {
-        console.log(paragraphData[index]);
+    else if (document.getElementById("projects") != null) {
+        var projectsParagraph = document.getElementById("projectsParagraph");
+        projectsParagraph.innerHTML = "These are some projects";
     }
-    */
-    
-    
-    /* even nicer but more complicated
-    paragraphData.forEach(function(paragraph) {
-        console.log(paragraph);
-    });
-    */
-  
-    //declared a named function way
-
-    function replaceFirstParagraph() {
-        console.log("inside replaceFirstParagraph function");
-        var firstParagraph;
-
-        firstParagraph = document.getElementById("firstParagraph");
-
-        firstParagraph.innerHTML = "My New Paragraph Data";
+    else if (document.getElementById("contact") != null) {
+        var email = document.getElementById("email");
+        var password = document.getElementById("password");
+        var checkMeOut = document.getElementById("checkMeOut");  
+        var submitButton = document.getElementById("submitButton");
         
+        // run event on checkbox check / uncheck
+        checkMeOut.addEventListener("change", function() {
+        //console.log(checkMeOut.checked);
+        
+        (checkMeOut.checked) ? console.log('checked') : console.log('unchecked');
+        });
+        
+        submitButton.addEventListener("click", function() {
+            event.preventDefault();
+           console.log("Email: " + email.value);
+           console.log("Password: " + password.value); 
+        });
     }
-
    
-    // call the replaceFirstParagraph function
-    replaceFirstParagraph();
-
 
 })();
 
